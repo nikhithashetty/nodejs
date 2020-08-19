bc79bc1aa31120d9d7a8edbed9f4a6a3fa7b6aa6
aaa <your@email.com>
2020-08-19 13:06:45 +0530

    message

---
+++ app.js
@@ -0,0 +1,80 @@
+const dialogflow = require('dialogflow');
+const uuid = require('uuid');
+const express=require('express')
+const bodyParse=require('body-parser')
+const app=express()
+const port=5000;
+const sessionId = uuid.v4();
+app.use(bodyParse.urlencoded({
+    extended:false
+}))
+app.use(function (req, res, next) {
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+  
+    // Pass to next layer of middleware
+    next();
+  });
+app.post('/send-msg',(req,res)=>{
+
+   runSample(req.body.data).then(data=>{
+        res.send({Reply:data})
+    })
+
+})
+/**
+ * Send a query to the dialogflow agent, and return the query result.
+ * @param {string} projectId The project to be used
+ */
+async function runSample(msg,projectId = 'ft-bal-jdnxov') {
+  console.log("A"+msg)
+  // A unconsole.lique identifier for the given session
+ 
+
+  // Create a new session
+  const sessionClient = new dialogflow.SessionsClient({
+      keyFilename:"D:/angular7/Chatbot_tutorial/FT-Bal-551c56a61f34.json"
+  });
+  const sessionPath = sessionClient.sessionPath(projectId, sessionId);
+
+  // The text query request.
+  const request = {
+    session: sessionPath,
+    queryInput: {
+      text: {
+        // The query to send to the dialogflow agent
+        text: msg,
+        // The language used by the client (en-US)
+        languageCode: 'en-US',
+      },
+    },
+  };
+var arr=[]
+  // Send request and log result
+  const responses = await sessionClient.detectIntent(request);
+  console.log('Detected intent');
+ 
+  const result = responses[0].queryResult;
+  for(var i=0;i<result.fulfillmentMessages.length;i++)
+  {
+    var x=result.fulfillmentMessages[i]
+    arr.push(x.text.text)
+  }
+  console.log(arr)
+  //console.log(`  Query: ${result.queryText}`);
+  //console.log(`  Response: ${result.fulfillmentText}`);
+  /*if (result.intent) {
+    console.log(`  Intent: ${result.intent.displayName}`);
+  } else {
+    console.log(`  No intent matched.`);
+  }*/
+  return arr;
+}
+
+app.listen(port,()=>{
+    console.log("running on port"+port)
+});
+
